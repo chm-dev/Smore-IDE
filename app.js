4f19844c3dbe58674ae9731db31727218446535a
TJDoesCode <trevr03@gmail.com>
2017-06-29 16:58:26 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const {
+  ipcRenderer
+} = require('electron');
+const loader = require('monaco-loader');
+const fs = require('fs');
+const Discord = require('discord.js');
+
+loader().then((monaco) => {
+  let editor = monaco.editor.create(document.querySelector('#container'), {
+    language: 'javascript',
+    theme: 'vs-dark',
+    automaticLayout: true
+  })
+
+  ipcRenderer.on('open-file', (e, url) => {
+    let data = fs.readFileSync(url.slice(7), 'utf-8')
+    editor.setModel(monaco.editor.createModel(data,
+      'javascript'))
+    console.log(data)
+  })
+})
---
+++ index.js
@@ -0,0 +1,16 @@
+const {
+  app,
+  BrowserWindow
+} = require('electron')
+
+app.on('ready', () => {
+  let mainWindow = new BrowserWindow()
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
+  mainWindow.webContents.openDevTools()
+
+  mainWindow.webContents.on('will-navigate', (e, url) => {
+    e.preventDefault();
+
+    mainWindow.webContents.send('open-file', url);
+  })
+})
