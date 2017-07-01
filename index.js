2922c94b5730e65a0fcc5e6666f7095c9bd323f3
Chronomly6 <michaelgummere@gmail.com>
2017-07-01 17:43:05 -0500

    add stuff

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const {
   app,
-  BrowserWindow
+  BrowserWindow,
+  Menu, 
+  Tray
 } = require('electron')
 
+let tray = null
 app.on('ready', () => {
   let mainWindow = new BrowserWindow()
   mainWindow.loadURL(`file://${__dirname}/index.html`)
   mainWindow.webContents.openDevTools()
+   tray = new Tray(`${__dirname}/icon.jpg`)
+  const contextMenu = Menu.buildFromTemplate([
+    {label: 'Smore-IDE'},
+    {label: 'Item3', type: 'radio', checked: true}
+  ])
+  tray.setToolTip('DO NOT DISTRIBUTE')
+  tray.setContextMenu(contextMenu)
 
   mainWindow.webContents.on('will-navigate', (e, url) => {
     e.preventDefault();
